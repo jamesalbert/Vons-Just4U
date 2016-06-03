f85540b0fbf507f6166dda3398b5576e2413ffe6
jamesalbert <jamesrobertalbert@gmail.com>
2016-06-03 10:19:41 -0700

    cleaner threads

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import threading
+import re
 import sys
-
-loginUrl = "https://www.vons.com/ShopStores/OSSO-Login.page?goto=http%3A%2F%2Fwww.vons.com%2FIFL%2FGrocery%2FHome"
-urls = ['http://www.vons.com/ShopStores/Justforu-PersonalizedDeals.page',
-        'http://www.vons.com/ShopStores/Justforu-CouponCenter.page']
-
-open('./output.log', 'w+').close()
 
 try:
     sys.argv.pop(0)
@@ -15,21 +14,54 @@ except:
     exit('you must give a username and password')
 
 
-def click_all(buttons):
-    for b in buttons:
-        try:
-            if 'Added' not in b.text:
-                b.click()
-                print 'added'
-        except Exception:
-            pass
-    return
+loginUrl = "https://www.vons.com/ShopStores/OSSO-Login.page?goto=http%3A%2F%2Fwww.vons.com%2FIFL%2FGrocery%2FHome"
+url = 'http://www.vons.com/ShopStores/Justforu-Coupons.page#/category/all'
+threadLock = threading.Lock()
 
-def addDeals():
-    '''
-    go through personalized and Just4U deals and add them
-    '''
+class CouponThread(threading.Thread):
+    def __init__(self, offer):
+        threading.Thread.__init__(self)
+        self.offer = offer
+    def run(self):
+        threadLock.acquire()
+        self.offer.click()
+        threadLock.release()
+
+class VonsHelper(object):
+    def __init__(self, driver):
+        self.driver = driver
+        self.savings = 0.00
+        self.threads = []
+
+    def handle_popups(self):
+        '''close change my store popup'''
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.ID, 'ssAdvancedDialogLeftButton'))
+            ).click()
+        except:
+            '''no popup opened'''
+            pass
+
+    def add_deals(self):
+        '''click all displayed (unadded) deals, go through 20 pages'''
+        for i in range(10):
+            self.driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+            WebDriverWait(self.driver, 3)
+        deals = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_all_elements_located((By.LINK_TEXT, 'Add')))
+        for deal in deals:
+            t = CouponThread(deal)
+            self.threads.append(t)
+            self.threads[-1].start()
+        for thread in self.threads:
+            thread.join()
+        print("you saved around ${:.2f}".format(self.savings))
+
+if __name__ == '__main__':
+    '''initialize browser'''
     driver = webdriver.Chrome()
+    helper = VonsHelper(driver)
     driver.get(loginUrl)
 
     '''login'''
@@ -39,32 +71,10 @@ def addDeals():
     psw.send_keys(password)
     driver.find_element_by_id('SignInBtn').click()
 
-    for url in urls:
-        driver.get(url)
-
-        '''close any popups'''
-        try:
-            driver.implicitly_wait(5)
-            driver.find_element_by_class_name('bt-close-button').click()
-        except:
-            '''no popup opened'''
-            pass
-
-        '''click all displayed (unadded) deals, go through 20 pages'''
-        for i in range(20):
-            deals = driver.find_elements_by_class_name('lt-place-add-button')
-            p = threading.Thread(target=click_all, args=(deals,))
-            p.start()
-            p.join()
-            driver.execute_script('window.scrollTo(0, document.body.scrollHeight / 2);')
-            driver.implicitly_wait(5)
+    '''add deals'''
+    driver.get(url)
+    helper.handle_popups()
+    helper.add_deals()
 
     '''close browser'''
-    driver.close()
-    out.write('deals added...\n')
-    out.close()
-
-if __name__ == '__main__':
-    out = open('output.log', 'a')
-    addDeals()
-    out.close()
+    driver.quit()
