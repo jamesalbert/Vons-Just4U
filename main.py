bdfb32bfe189779f860450e9ea52fe92dd79f384
jamesalbert <jalbert@cloudbees.com>
2018-07-15 12:46:13 -0700

    threads + py3.6 + venv

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 import threading
+import random
 import re
 import sys
+import time
 
 try:
     sys.argv.pop(0)
@@ -14,40 +17,69 @@ except:
     exit('you must give a username and password')
 
 
-loginUrl = "https://www.vons.com/ShopStores/OSSO-Login.page?goto=http%3A%2F%2Fwww.vons.com%2FIFL%2FGrocery%2FHome"
-url = 'http://www.vons.com/ShopStores/Justforu-Coupons.page#/category/all'
+storedId = 2229
+loginUrl = "https://www.safeway.com/ShopStores/OSSO-Login.page?goto=http%3A%2F%2Fwww.safeway.com%2FIFL%2FGrocery%2FHome"
+url = 'http://www.safeway.com/ShopStores/Justforu-Coupons.page#/category/all'
 threadLock = threading.Lock()
 
 class CouponThread(threading.Thread):
+
     def __init__(self, offer):
         threading.Thread.__init__(self)
         self.offer = offer
+
+
     def run(self):
         threadLock.acquire()
         self.offer.click()
+        time.sleep(1)
         threadLock.release()
 
+
 class VonsHelper(object):
+
     def __init__(self, driver):
         self.driver = driver
         self.savings = 0.00
         self.threads = []
 
+
     def handle_popups(self):
         '''close change my store popup'''
-        try:
-            WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.ID, 'ssAdvancedDialogLeftButton'))
-            ).click()
-        except:
-            '''no popup opened'''
-            pass
+        # try:
+        self.random_user_interaction()
+        store = WebDriverWait(self.driver, 10000).until(
+            EC.element_to_be_clickable((By.ID, 'ssAdvancedDialogLeftButton'))
+        )
+        if store:
+            store.click()
+            time.sleep(10)
+        # except:
+            # '''no popup opened'''
+            # pass
+
+
+    def random_user_interaction(self):
+        for i in range(20):
+            y = random.choice(["document.body.scrollHeight"] * 10 + ["0"])
+            print(f"scrolling to {y}")
+            self.driver.execute_script(f'window.scrollTo(0, {y});')
+
 
     def add_deals(self):
         '''click all displayed (unadded) deals, go through 20 pages'''
-        for i in range(10):
+        added = list()
+        unadded = list()
+        while len(unadded + added) < 400:
+            print(f'added: {len(added)}; unadded: {len(unadded)}')
+            try:
+                unadded = WebDriverWait(self.driver, 3).until(
+                    EC.presence_of_all_elements_located((By.LINK_TEXT, 'Add')))
+                added = WebDriverWait(self.driver, 3).until(
+                    EC.presence_of_all_elements_located((By.XPATH, "//div[text()='Added']")))
+            except:
+                pass
             self.driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-            WebDriverWait(self.driver, 3)
         deals = WebDriverWait(self.driver, 10).until(
             EC.presence_of_all_elements_located((By.LINK_TEXT, 'Add')))
         for deal in deals:
@@ -58,21 +90,21 @@ class VonsHelper(object):
             thread.join()
         print("you saved around ${:.2f}".format(self.savings))
 
+
 if __name__ == '__main__':
     '''initialize browser'''
     driver = webdriver.Chrome()
     helper = VonsHelper(driver)
-    driver.get(loginUrl)
+    # driver.get(loginUrl)
 
     '''login'''
-    uid = driver.find_element_by_name('userId')
+    driver.get(url)
+    uid = driver.find_element_by_name('input-email')
     uid.send_keys(username)
-    psw = driver.find_element_by_class_name('fakeiInputAlteredTextBox')
-    psw.send_keys(password)
-    driver.find_element_by_id('SignInBtn').click()
+    psw = driver.find_element_by_name('password-password')
+    psw.send_keys(password + Keys.ENTER)
 
     '''add deals'''
-    driver.get(url)
     helper.handle_popups()
     helper.add_deals()
 
